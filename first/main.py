3b87b22597d91a8b8544394de8db4a8f1bad8d57
7SoKKoS7 <97355065+7SoKKoS7@users.noreply.github.com>
2022-02-02 00:24:37 +0200

    add site on Flask

--- first/main.py
+++ first/main.py
@@ -1,7 +1,30 @@
-from flask import Flask
-
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+menu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "contact"}]
+
+
+@app.route("/")
+def index():
+    print( url_for('index'))
+    return render_template('index.html', menu=menu)
+
+@app.route("/about")
+def about():
+    print(url_for('about'))
+    return render_template('about.html', title='о сайте', menu=menu)
+
+@app.route("/profile/<username>")
+def profile(username):
+    return f"Пользователь: {username}"
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html', title="Обратная связь", menu = menu)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
